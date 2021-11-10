4a0d527d1d1837119b995fa38c96685b8e1368fe
Aaron Smith <aaronsmith1234@gmail.com>
2021-11-09 21:02:56 -0500

    black formatting and documentation

--- saseg_runner/runner.py
+++ saseg_runner/runner.py
@@ -13,31 +13,27 @@ import click
 import win32com.client
 
 SCRIPTDIR_PATH = Path(__file__).parent.resolve()
+DEFAULT_PROFILE_NAME = "SAS Asia"
+DEFAULT_EG_VERSION = "7.1"
 
 
 def run_egp(
-        egp_path: Union[str, Path],
-        profile_name: str = 'SAS Asia',
-        eg_version: str = '7.1',
-        overwrite: bool = False,
-        remove_log: bool = True,
-        verbose: bool = False,
-        ) -> None:
-    """
-    execute egp_path
+    egp_path: Union[str, Path],
+    profile_name: str = DEFAULT_PROFILE_NAME,
+    eg_version: str = DEFAULT_EG_VERSION,
+    overwrite: bool = False,
+    remove_log: bool = True,
+    verbose: bool = False,
+) -> None:
+    """[summary]
 
-    Parameters
-    ------------
-    egp_path : Union[str, Path]
-        SAS Enterprise Guide file path.
-    profile_name : str
-        profile name to use
-    overwrite: bool
-        controls whether to save the egp file after exection. if False, timestamp is added to filename. The default is False.
-    remove_log: bool
-        wether remove log files or not. the default is True.
-    verbose: bool
-        default is False
+    Args:
+        egp_path (Union[str, Path]): SAS Enterprise Guide file path.
+        profile_name (str, optional): profile name to use. Defaults to 'SAS Asia'.
+        eg_version (str, optional): Which version of EG to use. Defaults to '7.1'.
+        overwrite (bool, optional): controls whether to save the egp file after exection. if False, timestamp is added to filename. Defaults to False.
+        remove_log (bool, optional): Whether to remove log files or not. Defaults to True.
+        verbose (bool, optional): [description]. Defaults to False.
     """
     start_time = time.time()
 
@@ -56,71 +52,76 @@ def run_egp(
     print(f"elapsed_time:{elapsed_time}[sec]")
 
 
-
-
-
-def _check_egp_file_existence(egp_path:str)->Path:
+def _check_egp_file_existence(egp_path: str) -> Path:
     if not Path(egp_path).exists():
-        raise Exception(f'not found {egp_path}')
+        raise Exception(f"not found {egp_path}")
     egp_path = Path(egp_path).resolve()
     return egp_path
 
-def _open_enterprise_guide(eg_version:str)->win32com.client.CDispatch:
-    print(f'opening SAS Enterprise Guide {eg_version}')
-    app = win32com.client.Dispatch(f'SASEGObjectModel.Application.{eg_version}')
-    click.secho('-> application instance created', fg='green')
+
+def _open_enterprise_guide(eg_version: str) -> win32com.client.CDispatch:
+    print(f"opening SAS Enterprise Guide {eg_version}")
+    app = win32com.client.Dispatch(f"SASEGObjectModel.Application.{eg_version}")
+    click.secho("-> application instance created", fg="green")
     return app
 
-def _activate_enterprise_guide_profile(profile_name:str, app:win32com.client.CDispatch)->None:
-    print(f'activating profile:[{profile_name}]')
-    app.SetActiveProfile(profile_name)
-    click.secho(f'-> profile:[{profile_name}] activated', fg='green')
 
-def _open_egp(egp_path:Path, app:win32com.client.CDispatch):
-    print(f'opening {egp_path}')
+def _activate_enterprise_guide_profile(
+    profile_name: str, app: win32com.client.CDispatch
+) -> None:
+    print(f"activating profile:[{profile_name}]")
+    app.SetActiveProfile(profile_name)
+    click.secho(f"-> profile:[{profile_name}] activated", fg="green")
+
+
+def _open_egp(egp_path: Path, app: win32com.client.CDispatch):
+    print(f"opening {egp_path}")
     prjObject = app.Open(str(egp_path), "")
-    click.secho('-> egp file opened', fg='green')
+    click.secho("-> egp file opened", fg="green")
     return prjObject
 
-def _run_egp(egp_path:str, prjObject)->None:
-    print(f'running {egp_path}')
+
+def _run_egp(egp_path: str, prjObject) -> None:
+    print(f"running {egp_path}")
     prjObject.Run()
-    click.secho('-> run finished', fg='green')
+    click.secho("-> run finished", fg="green")
+
 
 def _check_log_for_errors(egp_path, log_dir):
     error_happend = False
-    for log in log_dir.rglob('*.log'):
-        with open(log, mode='r') as f:
+    for log in log_dir.rglob("*.log"):
+        with open(log, mode="r") as f:
             contents = f.read()
             error = re.search("^ERROR.*:", contents, re.MULTILINE)
-            if (error):
+            if error:
                 click.secho(f"[{log.stem}] failed in {egp_path.name}", fg="red")
                 start = error.start()
-                last = contents.find('\n', start)
+                last = contents.find("\n", start)
                 print(contents[start:last])
                 error_happend = True
                 break
     return error_happend
+
 
 def _save_and_close_egp(egp_path, overwrite, prjObject):
     output = ""
     if overwrite:
         output = egp_path
     else:
-        timestamp = datetime.datetime.now().strftime('%Y%m%d-%H%M')
-        output = str(egp_path.parent / ('.' + egp_path.stem + '_' + timestamp + '.egp'))
+        timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M")
+        output = str(egp_path.parent / ("." + egp_path.stem + "_" + timestamp + ".egp"))
 
     prjObject.SaveAs(output)
-    click.secho(f'-> saved to {output}', fg='green')
+    click.secho(f"-> saved to {output}", fg="green")
 
     prjObject.Close()
     return output
 
 
 def _retrieve_logs(eg_version, verbose, output):
-    print(f'getting logs from {output}')
-    log_dir = Path(f'./{Path(output).stem}_CodeAndLogs')
-    if (log_dir.exists()):
+    print(f"getting logs from {output}")
+    log_dir = Path(f"./{Path(output).stem}_CodeAndLogs")
+    if log_dir.exists():
         shutil.rmtree(log_dir)
     res = subprocess.run(
         f'Cscript "{SCRIPTDIR_PATH}/ExtractCodeAndLog.vbs" "{output}" "{eg_version}"',
@@ -130,15 +131,17 @@ def _retrieve_logs(eg_version, verbose, output):
     )
     if verbose:
         print(res.stdout)
-    click.secho('-> log created', fg='green')
+    click.secho("-> log created", fg="green")
     return log_dir
+
 
 def _finish_and_clean_up(egp_path, output, error_happend):
     if error_happend:
-        os.rename(output, str(Path(output).parent/(Path(output).stem+'.ERROR.egp')))
+        os.rename(output, str(Path(output).parent / (Path(output).stem + ".ERROR.egp")))
         raise SASEGRuntimeError("Your EGP File had ERROR in the logs; please review")
     else:
-        click.secho(f'successfully finished exectuing {egp_path.name}', fg='green')
+        click.secho(f"successfully finished exectuing {egp_path.name}", fg="green")
+
 
 def cli():
     run_egp(sys.argv[1])
@@ -151,4 +154,4 @@ class SASEGRuntimeError(Exception):
 if __name__ == "__main__":
     # simple test
     # run_egp(SCRIPTDIR_PATH.parent / 'tests/test_fail.egp')
-    run_egp(SCRIPTDIR_PATH.parent / 'tests/test_success.egp')
+    run_egp(SCRIPTDIR_PATH.parent / "tests/test_success.egp")
