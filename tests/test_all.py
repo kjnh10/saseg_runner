0e205b60873d548048914efeb628db3b1f630c3a
Aaron Smith <aaronsmith1234@gmail.com>
2021-11-13 13:41:11 -0500

    renaming for clarity

--- tests/test_all.py
+++ tests/test_all.py
@@ -2,7 +2,7 @@ import subprocess
 from pathlib import Path
 
 import pytest
-import saseg_runner as sas
+import saseg_runner as eg_runner
 
 
 SCRIPTDIR = Path(__file__).parent.resolve()
@@ -15,29 +15,29 @@ space_in_path_egp = SCRIPTDIR / "space exist/test_success.egp"
 
 
 def test_success():
-    sas.run_egp(str(success_egp), PROFILE_NAME)
+    eg_runner.run_egp(str(success_egp), PROFILE_NAME)
 
 
 def test_success_with_Path_object():
-    sas.run_egp(success_egp, PROFILE_NAME)
+    eg_runner.run_egp(success_egp, PROFILE_NAME)
 
 
 def test_japanese_char_in_path():
-    sas.run_egp(str(japanese_char_in_path_egp), PROFILE_NAME)
+    eg_runner.run_egp(str(japanese_char_in_path_egp), PROFILE_NAME)
 
 
 def test_space_in_path():
-    sas.run_egp(str(space_in_path_egp), PROFILE_NAME)
+    eg_runner.run_egp(str(space_in_path_egp), PROFILE_NAME)
 
 
 def test_fail():
-    with pytest.raises(sas.SASEGRuntimeError):
-        sas.run_egp(str(fail_egp), PROFILE_NAME)
+    with pytest.raises(eg_runner.SASEGRuntimeError):
+        eg_runner.run_egp(str(fail_egp), PROFILE_NAME)
 
 
 def test_fail_error_format2():  # there exists an error format like "ERROR 22-232: ......."
-    with pytest.raises(sas.SASEGRuntimeError):
-        sas.run_egp(str(SCRIPTDIR / "test_error_format2.egp"), PROFILE_NAME)
+    with pytest.raises(eg_runner.SASEGRuntimeError):
+        eg_runner.run_egp(str(SCRIPTDIR / "test_error_format2.egp"), PROFILE_NAME)
 
 
 def test_cli_success():
@@ -46,6 +46,6 @@ def test_cli_success():
 
 
 def test_eg_runner():
-    eg = sas.EGRunner(PROFILE_NAME)
+    eg = eg_runner.SASEGRunner(PROFILE_NAME)
     eg.run_egp(success_egp)
     eg.run_egp(success_egp)
